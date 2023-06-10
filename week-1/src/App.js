a94215f26f5810f1605741ce93a2c9639a4d07e5
Younghwan90s <younghwan1608@gmail.com>
2023-06-10 18:29:00 +0900

    JSX Grammar

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 
-function Son() {
-  return <div>I'm a Son</div>
+export default function App() {
+  const number = 1;
+  const pTagStyle = {
+    color: 'red',
+  }
+  return (
+    <div>
+      <p style={pTagStyle}>Hi React</p>
+      <p style={pTagStyle}>
+        {
+        number > 10
+          ? number + " is bigger than 10"
+          : number + " is less than 10"
+        }
+      </p>
+    </div>
+  );
 }
-
-function Mother() {
-  return <Son />
-}
-
-function GrandFather() {
-  return <Mother />
-}
-
-function App() {
-  return <GrandFather />
-}
-
-export default App;
