1f113fa5a404e040f0347ea0f8cb54fa0c44304d
Younghwan90s <younghwan1608@gmail.com>
2023-06-10 18:46:56 +0900

    Props & Props drilling

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React from 'react'
 
-export default function App() {
-  const number = 1;
-  const pTagStyle = {
-    color: 'red',
-  }
-  return (
-    <div>
-      <p style={pTagStyle}>Hi React</p>
-      <p style={pTagStyle}>
-        {
-        number > 10
-          ? number + " is bigger than 10"
-          : number + " is less than 10"
-        }
-      </p>
-    </div>
-  );
+function Son(props) {
+  return <div>I'm a {props.GrandFatherName}'s' grandchild</div>
 }
+
+function Mother(props) {
+  const name = "Janet"
+  const GrandFatherName = props.GrandFatherName
+  return <Son GrandFatherName={GrandFatherName} />
+}
+
+function GrandFather() {
+  const name = "Michael";
+  return <Mother GrandFatherName={name} />
+}
+
+function App() {
+  return <GrandFather />
+}
+
+export default App
