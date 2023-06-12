a282708f53e95168ad9d147bbaf4463460b42490
Younghwan90s <younghwan1608@gmail.com>
2023-06-12 16:24:12 +0900

    LogIn - State Prac

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 
 function App() {
-  // hook
-  const [fruit, setFruit] = useState("");
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const onIdChangeHandler = (event) => {
+    setId(event.target.value);
+  };
+
+  const onPWChangeHandler = (event) => {
+    setPw(event.target.value);
+  };
+
+  const alertAndRefreshInput = () => {
+    alert(`The Id you put is ${id} and the password is ${pw}`)
+    setId('')
+    setPw('')
+  }
 
   return (
     <div>
-      fruit:{" "}
-      <input
-        value={fruit}
-        onChange={function (event) {
-          setFruit(event.target.value);
-        }}
-      />
-      <br /> <br />
-      {fruit}
+      <div>
+        ID: <input type="text" value={id} onChange={onIdChangeHandler} />
+      </div>
+      <div>
+        PW: <input type="password" value={pw} onChange={onPWChangeHandler} />
+      </div>
+      <button onClick={alertAndRefreshInput}>LOGIN</button>
     </div>
   );
 }
