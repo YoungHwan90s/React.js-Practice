e13f980e31605fb37d183d44407b961eecb91ed0
Younghwan90s <younghwan1608@gmail.com>
2023-06-13 16:01:15 +0900

    React Hooks - useState

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -1,13 +1,28 @@
-import React from "react";
-import TestPage from "./components/TestPage";
-import GlobalStyle from "./GlobalStyle";
+import React, { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
-    <>
-      <GlobalStyle />
-      <TestPage title="it is title" contents="contents" />
-    </>
+    <div>
+      <div>number: {number}</div>
+      <button
+        onClick={() => {
+          // 기존 업데이트 방법 - 1만 올라감
+          // 배치 업데이트 한거번에 모아서 한 번만 반영)
+          // setNumber(number + 1);
+          // setNumber(number + 1);
+          // setNumber(number + 1);
+
+          // 함수형 업데이트 방법 (함수는 반환 값이 있어)
+          setNumber(currentNumber => currentNumber + 1);
+          setNumber(currentNumber => currentNumber + 1);
+          setNumber(currentNumber => currentNumber + 1);
+        }}
+      >
+        click
+      </button>
+    </div>
   );
 }
 
