9ea837aa1c2cb0b24456f6a564438cf7691026a8
Younghwan90s <younghwan1608@gmail.com>
2023-06-14 15:45:28 +0900

    React Hook - useRef example2

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -1,34 +1,20 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useEffect } from "react";
 
 function App() {
-  // ref에 저장한 값은 렌더링을 일으키지 않는다.
-  // 즉 초기화 되는 것을 막을 수 있다.
-  const [count, setCount] = useState(0);
-  const countRef = useRef(0);
-  const style = {
-    border: "1px solid black",
-    margin: "10px",
-    padding: "10px",
-  };
+  // 화면이 렌더링 될 때, 어떤 작업을 하고 싶다 : useEffect!
+  const idRef = useRef("");
 
-  const plusStateButtonHandler = () => {
-    setCount(count + 1);
-  };
-
-  const plusRefButtonHandler = () => {
-    countRef.current++;
-    console.log(countRef.current)
-  };
+  useEffect(() => {
+    idRef.current.focus();
+  }, []);
 
   return (
     <div>
-      <div style={style}>
-        state area {count} <br />
-        <button onClick={plusStateButtonHandler}>state increase</button>
+      <div>
+        id: <input type="text" ref={idRef} />
       </div>
-      <div style={style}>
-        ref area {countRef.current} <br />
-        <button onClick={plusRefButtonHandler}>ref increase</button>
+      <div>
+        pw: <input type="password" />
       </div>
     </div>
   );
