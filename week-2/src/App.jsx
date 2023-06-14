73fac738daba7c1e7983c2b21c057e853500c829
Younghwan90s <younghwan1608@gmail.com>
2023-06-14 18:44:45 +0900

    React Hooks - useMemo

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -1,38 +1,23 @@
-import React, { useState, useCallback } from "react";
-import Box1 from "./components/Box1";
-import Box2 from "./components/Box2";
-import Box3 from "./components/Box3";
+import "./App.css";
+import HeavyComponent from "./components/HeavyComponent";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const plusButtonHandler = () => {
-    setCount(count + 1);
+  const navStyleObj = {
+    backgroundColor: "yellow",
+    marginBottom: "30px",
   };
 
-  const minusButtonHandler = () => {
-    setCount(count - 1);
+  const footerStyleObj = {
+    backgroundColor: "green",
+    marginTop: "30px",
   };
-
-  const initCount = useCallback(() => {
-    console.log(`Changed from ${count} to 0`)
-    setCount(0)
-  }, [count])
 
   return (
-    <div>
-      <h3>Count example</h3>
-      <p>current Count: {count}</p>
-      <button onClick={plusButtonHandler}>+</button>
-      <button onClick={minusButtonHandler}>-</button>
-      <div style={{
-        display: "flex",
-        marginTop: "10px"
-      }}>
-        <Box1 initCount={initCount}/>
-        <Box2 />
-        <Box3 />
-      </div>
-    </div>
+    <>
+      <nav style={navStyleObj}>네비게이션 바</nav>
+      <HeavyComponent />
+      <footer style={footerStyleObj}>푸터 영역이에요</footer>
+    </>
   );
 }
 
