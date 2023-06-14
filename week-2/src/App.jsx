853c45a260dc9a483ca530cd2676073f4b878caa
Younghwan90s <younghwan1608@gmail.com>
2023-06-14 15:58:06 +0900

    LogIn - useState & useRef & focus()

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -1,20 +1,36 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 function App() {
   // 화면이 렌더링 될 때, 어떤 작업을 하고 싶다 : useEffect!
   const idRef = useRef("");
+  const pwRef = useRef("");
+  const [id, setId] = useState("");
 
   useEffect(() => {
     idRef.current.focus();
   }, []);
 
+  useEffect(() => {
+    if (id.length >= 10) {
+      pwRef.current.focus();
+    }
+  }, [id]);
+
   return (
     <div>
       <div>
-        id: <input type="text" ref={idRef} />
+        id:{" "}
+        <input
+          value={id}
+          onChange={(event) => {
+            setId(event.target.value);
+          }}
+          type="text"
+          ref={idRef}
+        />
       </div>
       <div>
-        pw: <input type="password" />
+        pw: <input type="password" ref={pwRef} />
       </div>
     </div>
   );
