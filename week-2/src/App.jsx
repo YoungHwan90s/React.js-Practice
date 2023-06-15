320cb0bb2c70138ee3a59e9f2ecdb283e4f2a135
Younghwan90s <younghwan1608@gmail.com>
2023-06-15 17:40:26 +0900

    Redux action creator - payload

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { minusOne, plusOne } from "./redux/modules/counter";
+import { minusN, plusN } from "./redux/modules/counter";
 
 function App() {
   const counter = useSelector((state) => {
     return state.counter;
   });
 
+  const [number, setNumber] = useState("");
+
   // disPatch 가져오는법
+  // action 객체를 store로 dispatch가 던진다! 개념
   const dispatch = useDispatch();
 
-  console.log("counter -> ", counter.number);
   return (
     <div>
       <div>Current Count: {counter.number}</div>
+      <div>
+        <input
+          value={number}
+          onChange={(event) => setNumber(+event.target.value)}
+          type="number"
+        />
+      </div>
       <button
         onClick={() => {
-          dispatch(plusOne());
+          dispatch(plusN(number));
         }}
       >
         +
       </button>
       <button
         onClick={() => {
-          dispatch(minusOne());
+          dispatch(minusN(number));
         }}
       >
         -
--- week-2/src/redux/modules/counter.js
+++ week-2/src/redux/modules/counter.js
@@ -1,17 +1,33 @@
 // action value
-export const PLUS_ONE = "counter/PLUS_ONE";
-export const MINUS_ONE = "counter/MINUS_ONE";
+// const PLUS_ONE = "counter/PLUS_ONE";
+// const MINUS_ONE = "counter/MINUS_ONE";
+const PLUS = "counter/PLUS";
+const MINUS = "conter/MINUS";
 
 // action creator : action value를 return 하는 함수
-export const plusOne = () => {
+// export const plusOne = () => {
+//   return {
+//     type: PLUS_ONE,
+//   };
+// };
+
+// export const minusOne = () => {
+//   return {
+//     type: MINUS_ONE,
+//   };
+// };
+
+export const plusN = (payload) => {
   return {
-    type: PLUS_ONE,
+    type: PLUS,
+    payload,
   };
 };
 
-export const minusOne = () => {
+export const minusN = (payload) => {
   return {
-    type: MINUS_ONE,
+    type: MINUS,
+    payload,
   };
 };
 
@@ -21,19 +37,27 @@ const initialState = {
 };
 
 // 리듀서
-// - 'statedㅔ 변화를 일으키는 '함수
+// - 'state에 변화를 일으키는 '함수
 // - state를 action의 type에 따라 변경하는 함수
 
 // input : state & action
 const counter = (state = initialState, action) => {
   switch (action.type) {
-    case PLUS_ONE:
+    // case PLUS_ONE:
+    //   return {
+    //     number: state.number + 1,
+    //   };
+    // case MINUS_ONE:
+    //   return {
+    //     number: state.number - 1,
+    //   };
+    case PLUS:
       return {
-        number: state.number + 1,
+        number: state.number + action.payload,
       };
-    case MINUS_ONE:
+    case MINUS:
       return {
-        number: state.number - 1,
+        number: state.number - action.payload,
       };
     default:
       return state;
