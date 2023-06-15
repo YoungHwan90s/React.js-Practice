fe19afa4cc0cb1b3e84ac8eaf8fdb82058f239fb
Younghwan90s <younghwan1608@gmail.com>
2023-06-15 17:46:54 +0900

    Redux Ducks Pattern(action type, creator, reducer)

--- week-2/src/redux/modules/counter.js
+++ week-2/src/redux/modules/counter.js
@@ -1,22 +1,12 @@
-// action value
+// DUCKS PATTERN - 1 module file(Action Type, Action Creator, Reducer)
+
+// 1. action type
 // const PLUS_ONE = "counter/PLUS_ONE";
 // const MINUS_ONE = "counter/MINUS_ONE";
 const PLUS = "counter/PLUS";
 const MINUS = "conter/MINUS";
 
-// action creator : action value를 return 하는 함수
-// export const plusOne = () => {
-//   return {
-//     type: PLUS_ONE,
-//   };
-// };
-
-// export const minusOne = () => {
-//   return {
-//     type: MINUS_ONE,
-//   };
-// };
-
+// 2. action creator : action value를 return 하는 함수
 export const plusN = (payload) => {
   return {
     type: PLUS,
@@ -31,26 +21,18 @@ export const minusN = (payload) => {
   };
 };
 
-// 초기 상태값(state)
+// 3. 초기 상태값(state) SET
 const initialState = {
   number: 0,
 };
 
-// 리듀서
+// 4. Reducer
 // - 'state에 변화를 일으키는 '함수
 // - state를 action의 type에 따라 변경하는 함수
+// - input : ALWAYS state & action
 
-// input : state & action
 const counter = (state = initialState, action) => {
   switch (action.type) {
-    // case PLUS_ONE:
-    //   return {
-    //     number: state.number + 1,
-    //   };
-    // case MINUS_ONE:
-    //   return {
-    //     number: state.number - 1,
-    //   };
     case PLUS:
       return {
         number: state.number + action.payload,
