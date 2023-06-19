16c9702f8844f750f4483be4ec0a49ad7fe85712
Younghwan90s <younghwan1608@gmail.com>
2023-06-19 19:57:56 +0900

    json-server, HTTP, axios(get)

--- week-3/src/App.jsx
+++ week-3/src/App.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
-  return <div>test-page</div>;
+  const [todos, setTodos] = useState(null);
+
+  const fetchTodos = async () => {
+    const { data } = await axios.get("http://localhost:3001/todos");
+
+    setTodos(data);
+    console.log(todos);
+  };
+
+  useEffect(() => {
+    // db로부터 값을 가져올 것
+    fetchTodos();
+  }, []);
+
+  return (
+    <div>
+      {todos?.map((item) => {
+        return (
+          <div key={item.id}>
+            {item.id}:{item.title}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
