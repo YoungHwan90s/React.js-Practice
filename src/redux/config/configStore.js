eade9001e49e544b6e273c7f1c98bd526adcdf17
Younghwan90s <younghwan1608@gmail.com>
2023-06-19 15:52:24 +0900

    update

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,14 +1,17 @@
-import { createStore } from "redux";
-import { combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import todos from "../modules/todos";
 
 // 1. create rootReducer with reducers
-const rootReducer = combineReducers({
-  todos,
-});
+// const rootReducer = combineReducers({
+//   todos,
+// });
 
 // 2. create store
-const store = createStore(rootReducer);
+const store = configureStore({
+  reducer: {
+    todos,
+  },
+});
 
 // 3. export
 export default store;
