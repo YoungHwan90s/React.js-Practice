bd2568ac984f4a57a736d42223b146d8ae762a5f
Younghwan90s <younghwan1608@gmail.com>
2023-06-20 20:59:14 +0900

    json-server, axios(post, delete, fetch)

--- week-3/src/App.jsx
+++ week-3/src/App.jsx
@@ -3,12 +3,49 @@ import axios from "axios";
 
 function App() {
   const [todos, setTodos] = useState(null);
+  const [inputValue, setInputValue] = useState({
+    title: "",
+  });
+  const [targetId, setTargetId] = useState("");
+  const [contents, setContents] = useState("");
 
+  // 조회 함수
   const fetchTodos = async () => {
     const { data } = await axios.get("http://localhost:3001/todos");
-
     setTodos(data);
-    console.log(todos);
+  };
+
+  // 추가 함수
+  const onSubmitHandler = async () => {
+    await axios.post("http://localhost:3001/todos", inputValue);
+    // setTodos([...todos, inputValue]);
+    fetchTodos();
+  };
+
+  // 삭제 함수
+  const onDeleteButtonHandler = async (id) => {
+    axios.delete(`http://localhost:3001/todos/${id}`);
+    setTodos(
+      todos.filter((item) => {
+        return item.id !== id;
+      })
+    );
+  };
+
+  // 수정 함수
+  const onUpdateButtonHandler = async () => {
+    axios.patch(`http://localhost:3001/todos/${targetId}`, {
+      title: contents,
+    });
+    setTodos(
+      todos.map((item) => {
+        if (item.id == targetId) {
+          return { ...item, title: contents };
+        } else {
+          return item;
+        }
+      })
+    );
   };
 
   useEffect(() => {
@@ -17,15 +54,61 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {todos?.map((item) => {
-        return (
-          <div key={item.id}>
-            {item.id}:{item.title}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div>
+        {/* 수정 영역 */}
+        <input
+          type="text"
+          placeholder="수정할 아이디"
+          value={targetId}
+          onChange={(e) => {
+            setTargetId(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="수정할 내용"
+          value={contents}
+          onChange={(e) => {
+            setContents(e.target.value);
+          }}
+        />
+        <button onClick={onUpdateButtonHandler}>수정</button>
+        <br />
+        <br />
+      </div>
+      <div>
+        {/* INPUT 영역 */}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSubmitHandler();
+            // 버튼 클릭시, inputㅇ 들어있는 값(state)을 이용하여 db에 저장
+          }}
+        >
+          <input
+            type="text"
+            value={inputValue.title}
+            onChange={(e) => {
+              setInputValue({ title: e.target.value });
+            }}
+          />
+          <button type="submit">추가</button>
+        </form>
+      </div>
+      <div>
+        {/* 데이터 영역 */}
+        {todos?.map((item) => {
+          return (
+            <div key={item.id}>
+              {item.id}:{item.title}
+              &nbsp;
+              <button onClick={() => onDeleteButtonHandler(item.id)}>X</button>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
